38d5cba6dbf4bd4e5f8f26d17f5df02e43f5fef6
Ilya Rodionov <ris58h@gmail.com>
2018-04-03 19:16:57 +0300

    Remove commented code.

--- content.js
+++ content.js
@@ -1,10 +1,5 @@
 let previousState = null;
 
-// function printDebugInfo(type) {//TODO
-//     console.log(type);
-//     console.log(previousState);
-// }
-
 let unscrolling = false;
 
 let beforeScrollState = state();
@@ -19,11 +14,9 @@ document.addEventListener("scroll", () => {
     const newTimestamp = Date.now();
 
     const interval = (newTimestamp - beforeScrollTimestamp) / 1000;
-    // console.log("interval: " + interval);
     // Usualy interval is around 0.02 but there is a some strange one-time delay if spacebar is being pressed.
     if (interval > 0.1) {
         previousState = beforeScrollState;
-        // printDebugInfo("changed state");
     }
 
     beforeScrollState = newState;
@@ -38,7 +31,6 @@ document.addEventListener('keyup', function(e) {
             if (previousState != null) {
                 const s = state();
                 if (s.x != previousState.x || s.y != previousState.y) {
-                    // printDebugInfo("before unscroll");
                     unscrolling = true;
     
                     window.scrollTo(previousState.x, previousState.y);
